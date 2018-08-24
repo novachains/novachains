e597913578bb326c6650568899772191714e9a9c
x56080 <sir_xujianhui@163.com>
2018-08-24 21:54:43 +0800

    update code

---
+++ src/programs/novad/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main( int argc, char *agrv[] )
+{
+   return 0 ;
+}
--- src/test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-
-int main( int, char** )
-{
-   printf( "Hello world\n" ) ;
-   return 0 ;
-}
