137974645bfa8fc8925d919f6206d7e3ce079732
Jun Tang <thinkingblock@hotmail.com>
2018-09-11 03:30:39 -0400

    Nova client update

--- src/include/ossNPipe.hpp
+++ src/include/ossNPipe.hpp
@@ -107,7 +107,7 @@ typedef class _OSSNPIPE OSSNPIPE ;
 #define OSS_NPIPE_PREFIX              "\\\\"
 #define OSS_NPIPE_LOCAL_PREFIX        OSS_NPIPE_PREFIX".\\pipe\\"
 #else
-#define OSS_NPIPE_LOCAL_PREFIX        "/var/sequoiadb"
+#define OSS_NPIPE_LOCAL_PREFIX        "/var/nova"
 #endif
 
 #define OSS_NPIPE_INBOUND            0x00000001
